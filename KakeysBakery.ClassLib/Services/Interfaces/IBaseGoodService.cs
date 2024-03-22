8150f709e9867743c5cc44857a06845422cd6a89
kalin <kalin@DESKTOP-T0IAO7R>
2024-03-22 13:16:40 -0600

    added some order functionality

--- KakeysBakery.ClassLib/Services/Interfaces/IBaseGoodService.cs
+++ KakeysBakery.ClassLib/Services/Interfaces/IBaseGoodService.cs
@@ -8,7 +8,10 @@ public interface IBaseGoodService
     public Task<List<Basegood>> GetBasegoodsFromTypeAsync(int BaseGoodTypeId);
     public Task<List<Basegood>> GetBaseGoodListAsync();
     public Task<Basegood?> GetBaseGoodAsync(int id);
-    public Task CreateBaseGoodAsync(Basegood basegood);
+    public Task<Basegood?> GetBaseGoodFromFlavorAsync(int id, int flavorid);
+
+
+	public Task CreateBaseGoodAsync(Basegood basegood);
     public Task DeleteBaseGoodAsync(int basegoodId);
     public Task UpdateBaseGoodAsync(Basegood basegood);
 }
