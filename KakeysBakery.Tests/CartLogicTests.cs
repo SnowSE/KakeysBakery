48ebde92c62c032df1392218571b7c9d612be70a
aaron.allen <aaron.allen@ENGRHJCP5S3>
2024-04-04 13:10:54 -0600

    Added some cart logic in a cs file. so far from done

---
+++ KakeysBakery.Tests/CartLogicTests.cs
@@ -0,0 +1,99 @@
+﻿using KakeysBakery.Services.nonDBServices;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KakeysBakeryTests;
+
+public class CartLogicTests : IClassFixture<BakeryFactory>
+{
+    public HttpClient client { get; set; }
+    public CartLogicTests(BakeryFactory Factory)
+    {
+        client = Factory.CreateDefaultClient();
+
+    }
+
+
+    [Fact]
+    public async Task CanAddFinishedProductToCart()
+    {
+        ///TODO this is the big test or the goal
+        //// ARRANGE
+        //CartLogic testCart = new CartLogic(client);
+
+        //Basegood testGood = new Basegood()
+        //{
+        //    Id = 2000,
+        //};
+        ////Product testProduct = new Product()
+        ////{
+        ////    Id = 4000
+        ////};
+        ////ProductAddonBasegood finishedProduct = new ProductAddonBasegood()
+        ////{
+        ////    Id = 3000,
+        ////    Productid = 4000
+        ////};
+        //Customer testCustomer = new Customer()
+        //{
+        //    Id = 5000
+        //};
+
+        ////Act
+        //Product productInCart = await testCart.addBaseGoodAsync(testGood, testCustomer);
+        //List<Cart>? carts = await client.GetFromJsonAsync<List<Cart>>("api/cart/getall");
+        //Cart cart = carts.FirstOrDefault(c => c.Productid == productInCart.Id);
+        ////Cart cart = await client.GetFromJsonAsync<Cart>("api/Cart/get")
+
+        ////Assert
+        //Assert.NotNull(cart);
+
+        ////Assert.NotNull(client);
+    }
+
+    [Fact]
+    public async Task GivenBaseGoodExists_WhenBaseGoodIsNotALoneProduct_ThenCreateProductWithBaseGood()
+    {
+        //TODO
+    }
+
+    [Fact]
+    public async Task CanCheckIfProductIsALoneProduct()
+    {
+        CartLogic testCart = new CartLogic(client);
+        Product testProduct = new Product() { Id = 1000 };
+        Basegood testgood = new Basegood() { Id = 2000 };
+        ProductAddonBasegood link = new ProductAddonBasegood()
+        {
+            Id = 3000,
+            Productid = testProduct.Id,
+            Basegoodid = testgood.Id
+        };
+
+        await client.PostAsJsonAsync("api/basegood/add", testgood);
+        await client.PostAsJsonAsync("api/product/add", testProduct);
+        await client.PostAsJsonAsync("api/productAddonBasegood/add", link);
+
+
+        int result = await testCart.FindProductForSingleBaseGoodAsync(testgood.Id);
+        Assert.Equal(testProduct.Id, result);
+    }
+
+
+    [Fact]
+    public async Task WhenGivenNonExistingBaseGood_ThenWillThrowException()
+    {
+        Customer testCustomer = new Customer() { Id = 23456};
+        Basegood basegood = new Basegood() { Id = 1234567890 };
+        CartLogic testCart = new CartLogic(client);
+        await Assert.ThrowsAsync<InputFormatterException>(async () =>
+        {
+            await testCart.addBaseGoodAsync(basegood, testCustomer);
+        });
+    }
+}
---
+++ KakeysBakery/Services/nonDBServices/CartLogic.cs
@@ -0,0 +1,70 @@
+﻿using AngleSharp.Diffing.Strategies.AttributeStrategies;
+using KakeysBakery.Components;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace KakeysBakery.Services.nonDBServices;
+public class CartLogic
+{
+    HttpClient client;
+    public CartLogic(HttpClient Client)
+    {
+        client = Client;
+    }
+    public async Task<Product> addBaseGoodAsync(Basegood addBaseGood, Customer customer)
+    {
+        if (await baseGoodExistsAsync(addBaseGood.Id) == false) throw new InputFormatterException();
+        await client.PostAsJsonAsync("api/Customer/add", customer);
+        await client.PostAsJsonAsync("api/Basegood/add", addBaseGood);
+        //await client.PostAsJsonAsync("api/Product/add", testProduct);
+        //await client.PostAsJsonAsync("api/ProductAddonBasegood/add", finishedProduct);
+
+        return new Product();
+    }
+
+    public async Task<int> FindProductForSingleBaseGoodAsync(int basegoodId)
+    {
+        List<ProductAddonBasegood> result = await client.GetFromJsonAsync<List<ProductAddonBasegood>>("api/productAddonBasegood/getall");
+        
+        //where there is a line that exists without an addon
+        ProductAddonBasegood? result2 = result
+            .Where(c => c.Basegoodid == basegoodId)
+            .Where(c => c.Addonid is null).FirstOrDefault();
+        
+        //all the lines with addons and the same basegood
+        List<ProductAddonBasegood> result3
+             = result
+            .Where(c => c.Basegoodid == basegoodId)
+            .Where(c => c.Addonid is not null).ToList();
+
+        //if this basegood is the only thing attached to the product
+        if (result2 is not null && result3.Count == 0)
+        { return (int)result2.Productid!; }
+
+        //if the product is not connected, connect it
+        else
+        {
+            Product product = new Product();
+            await client.PostAsJsonAsync("api/product/add", product);
+            ProductAddonBasegood productAddon = new ProductAddonBasegood()
+            {
+                Basegoodid = basegoodId,
+                Productid = product.Id,
+            };
+            await client.PostAsJsonAsync("api/productAddonBasegood/add", productAddon);
+
+            return product.Id;
+        }
+    }
+
+    private async Task<bool> baseGoodExistsAsync(int basegoodId)
+    {
+        Basegood? result = null;
+        try
+        {
+            result  = await client.GetFromJsonAsync<Basegood>($"api/basegood/get/{basegoodId}");
+        }
+        catch { return false; }
+        if (result == null) return false;
+        else return true;
+    }
+}
