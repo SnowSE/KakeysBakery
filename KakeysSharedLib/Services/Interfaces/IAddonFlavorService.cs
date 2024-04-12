7a8765b38742afbdb7087360eb2f37dd5bf0a496
brycecoon <112597142+brycecoon@users.noreply.github.com>
2024-04-12 11:42:41 -0600

    Maui works, don't use authentication

---
+++ KakeysSharedLib/Services/Interfaces/IAddonFlavorService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using KakeysBakeryClassLib.Data;
+
+namespace KakeysBakeryClassLib.Services.Interfaces;
+
+public interface IAddonFlavorService
+{
+    public Task<List<Addonflavor>> GetAddonFlavorListAsync();
+    public Task<Addonflavor?> GetAddonFlavorAsync(int id);
+    public Task CreateAddonFlavorAsync(Addonflavor addonFlavor);
+    public Task DeleteAddonFlavorAsync(int addonFlavorId);
+    public Task UpdateAddonFlavorAsync(Addonflavor addonFlavor);
+    public Task<Addonflavor?> GetAddonFlavorByFlavorAsync(string flavor);
+}
---
+++ KakeysSharedLib/Services/Interfaces/nonDBInterfaces/ICartLogic.cs
@@ -0,0 +1,14 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//using KakeysBakeryClassLib.Data;
+
+//namespace KakeysBakeryClassLib.Services.Interfaces.nonDBInterfaces;
+
+//public interface ICartLogic
+//{
+//    public Product OrderProduct();
+//}
