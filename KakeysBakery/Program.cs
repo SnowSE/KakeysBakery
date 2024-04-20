6b339e0cf44aae47ea294858b85b8a2f1abb7267
Garion Cox <garion.cox@students.snow.edu>
2024-04-19 18:01:07 -0600

    Added fallback base uri for httpclient in webapp

--- KakeysBakery/Program.cs
+++ KakeysBakery/Program.cs
@@ -91,7 +91,7 @@ builder.Services.AddScoped(o =>
     var client = new HttpClient
     {
         //BaseAddress = new Uri("https://localhost:7196")
-        BaseAddress = new Uri(builder.Configuration["BaseUri"]!)
+        BaseAddress = new Uri(builder.Configuration["BaseUri"] ?? "https://kakeysbakery20240319120850.azurewebsites.net/")
     };
     return client;
 });
